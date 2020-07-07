8e03b0c5cc269ce1bb51b01f70a9aa36496d0e01
Administrator <admin@example.com>
2020-07-07 17:51:18 +0800

    增加genuuid方法

--- function.go
+++ function.go
@@ -1,5 +1,10 @@
 package tr_ivy_common
 
+import (
+	"github.com/gofrs/uuid"
+	"github.com/mr-tron/base58"
+)
+
 // 获取复合设备标识
 func GetDeviceComboId(deviceId string, devicePlatform string) (deviceComboId string) {
 	if deviceId != "" && devicePlatform != "" {
@@ -8,4 +13,24 @@ func GetDeviceComboId(deviceId string, devicePlatform string) (deviceComboId str
 	} else {
 		return ""
 	}
+}
+
+/*
+	高并发情况下，生成全局唯一id
+	1、注意幂等性且全局唯一
+	2、注意安全性，不能被猜疑破解
+	3、趋势递增型
+	比如订单号 ：业务编码 + 时间戳 + 机器编码（前四位） + 随机数（四位） + 毫秒
+    采用uuid1+uuid4并压缩连接构成, 和python管理后台一致
+*/
+
+func GenUuid() string {
+	/*
+		uuid 组成部分：当前日期和时间 + 时钟序列 + 随机数 + 全球唯一的IEEE识别码
+		全球唯一的IEEE识别码 ：从网卡mac 中获得，没有网卡尝试从其他地方获得
+	*/
+	// 创建
+	id1, _ := uuid.NewV1() // based on timestamp and MAC address (RFC 4122)
+	id4, _ := uuid.NewV4() // Version 4, based on random numbers (RFC 4122)
+	return base58.Encode(id1.Bytes()) + base58.Encode(id4.Bytes())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/hyperknob/tr_ivy_common
 
 go 1.12
 
-require github.com/gbrlsnchs/jwt/v3 v3.0.0-rc.2
+require (
+	github.com/gbrlsnchs/jwt/v3 v3.0.0-rc.2
+	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/mr-tron/base58 v1.2.0
+)
