e94604a5c83d20c9281edef26aef30fd319a9349
Administrator <admin@example.com>
2020-09-19 18:43:13 +0800

    修改接口日志记录方法

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/hyperknob/tr_ivy_common
 go 1.12
 
 require (
+	github.com/astaxie/beego v1.12.2
 	github.com/gbrlsnchs/jwt/v3 v3.0.0-rc.2
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/mitchellh/hashstructure v1.0.0
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mr-tron/base58 v1.2.0
 	github.com/shomali11/util v0.0.0-20200329021417-91c54758c87b
-	github.com/spaolacci/murmur3 v1.1.0 // indirect
-	github.com/willf/bitset v1.1.10 // indirect
-	github.com/willf/bloom v2.0.3+incompatible
 	github.com/wonderivan/logger v1.0.0
 )
--- utils/logutil.go
+++ utils/logutil.go
@@ -3,17 +3,26 @@ package utils
 import (
 	"encoding/json"
 	"github.com/wonderivan/logger"
+	"github.com/astaxie/beego"
 )
 
 /**
  * 为logStash记录的Beego接口日志
  */
-func RecordLog4LogStash(methodName string, methodType string, methodProtocol, input interface{}) {
+func RecordLog4LogStash(methodType string, body interface{}, c *beego.Controller) {
 	recordVal := make(map[string]interface{})
-	recordVal["methodName"] = methodName
-	recordVal["methodType"] = methodType
-	recordVal["methodProtocol"] = methodProtocol
-	recordVal["parameters"] = input
+	recordVal["controllerName"], recordVal["actionName"] = c.GetControllerAndAction()
+	recordVal["actionType"] = methodType
+	recordVal["protocol"] = c.Ctx.Request.Proto
+	recordVal["routePath"] = c.Ctx.Request.RequestURI
+	if methodType == "req" {
+		recordVal["header"] = c.Ctx.Request.Header
+		recordVal["remoteAddr"] = c.Ctx.Request.RemoteAddr
+	} else if methodType == "res" {
+		c.Ctx.Output.Header("X-Request-Id", c.Ctx.Input.Header("X-Request-Id"))
+		recordVal["header"] = c.Ctx.ResponseWriter.Header()
+	}
+	recordVal["body"] = body
 	recordValBytes, _ := json.Marshal(recordVal)
 	logger.Info(string(recordValBytes))
 }
