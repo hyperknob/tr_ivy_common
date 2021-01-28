5189f92c46d843af22e8d4da56c19b7be88995a3
Administrator <admin@example.com>
2021-01-28 22:00:16 +0800

    增加更多日志方法

--- utils/logutil.go
+++ utils/logutil.go
@@ -26,3 +26,33 @@ func RecordLog4LogStash(methodType string, body interface{}, c beego.Controller)
 	recordValBytes, _ := json.Marshal(recordVal)
 	logger.Info(string(recordValBytes))
 }
+
+/**
+ * 为logStash记录的普通日志
+ */
+func Info(body string) {
+	recordVal := make(map[string]interface{})
+	recordVal["body"] = body
+	recordValBytes, _ := json.Marshal(recordVal)
+	logger.Info(string(recordValBytes))
+}
+
+/**
+ * 为logStash记录的警告日志
+ */
+func Warn(body interface{}) {
+	recordVal := make(map[string]interface{})
+	recordVal["body"] = body
+	recordValBytes, _ := json.Marshal(recordVal)
+	logger.Warn(string(recordValBytes))
+}
+
+/**
+ * 为logStash记录的错误日志
+ */
+func Error(body interface{}) {
+	recordVal := make(map[string]interface{})
+	recordVal["body"] = body
+	recordValBytes, _ := json.Marshal(recordVal)
+	logger.Error(string(recordValBytes))
+}
