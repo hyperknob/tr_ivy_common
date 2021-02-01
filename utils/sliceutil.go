ff629c591ebb2504de1eabb0965f363f325a3df7
Administrator <admin@example.com>
2021-02-01 23:04:02 +0800

    加入去重方法

--- utils/sliceutil.go
+++ utils/sliceutil.go
@@ -96,3 +96,17 @@ func IntItemIsContain(items []int, item int) bool {
 	}
 	return false
 }
+
+// 去重
+func Distinct(slc []string) []string {
+	result := []string{}
+	tempMap := map[string]byte{}  // 存放不重复主键
+	for _, e := range slc {
+		l := len(tempMap)
+		tempMap[e] = 0
+		if len(tempMap) != l{  // 加入map后，map长度变化，则元素不重复
+			result = append(result, e)
+		}
+	}
+	return result
+}
